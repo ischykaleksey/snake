91ae0d21acabeab97f7a12d7521fa5a5ea00da33
ischykaleksey <ischyk_aleksey@mail.ru>
2018-12-25 12:38:40 +0700

    Добавлены конструкторы

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point pnt1 = new Point();
-            pnt1.x = 2;
-            pnt1.y = 3;
-            pnt1.symbol = '*';
+            Point pnt1 = new Point(2,3,'*');
+            //pnt1.x = 2;
+            //pnt1.y = 3;
+            //pnt1.symbol = '*';
             pnt1.Draw();
 
-            Point pnt2 = new Point();
-            pnt2.x = 4;
-            pnt2.y = 5;
-            pnt2.symbol = '#';
+            Point pnt2 = new Point(4,5,'#');
+            //pnt2.x = 4;
+            //pnt2.y = 5;
+            //pnt2.symbol = '#';
             pnt2.Draw();
 
             Console.ReadLine();
